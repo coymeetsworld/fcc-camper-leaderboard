4d375b96610e6796e8087da07e9948b8080e61e6
Coy Sanders <coymeetsworld@gmail.com>
2017-01-04 02:13:49 -0800

    Able to render top 100 recent camper names after mounting of Leaderboard component.

--- app/api/FCCLeaderboardAPI.jsx
+++ app/api/FCCLeaderboardAPI.jsx
@@ -7,14 +7,18 @@ module.exports = {
 
 	getTop100Recent() {
 		return axios.get(FCC_TOP_100_RECENT_URL).then((res) => {
-			console.log(res);	
+			if(res.status !== 200) { throw new Error(res.statusText);	}
+			console.log("Success");
+			return res.data;
 		}, (err) => {
 			throw new Error(err.response.data.message);	
 		});
 	},
 	getTop100AllTime() {
 		return axios.get(FCC_TOP_100_ALL_TIME_URL).then((res) => {
-			console.log(res);	
+			if(res.status !== 200) { throw new Error(res.statusText); }
+			console.log("Success");
+			return res.data;
 		}, (err) => {
 			throw new Error(err.response.data.message);	
 		});
--- app/components/Leaderboard.jsx
+++ app/components/Leaderboard.jsx
@@ -5,20 +5,34 @@ class Leaderboard extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = { campers : [] };
+
 	}
 	
-	componentDidMount() {			
-		console.log("hello");
-		FCCLeaderboardAPI.getTop100Recent();
-		FCCLeaderboardAPI.getTop100AllTime();
+	componentDidMount () {
+		FCCLeaderboardAPI.getTop100Recent().then((res) => {
+			//console.log("Top 100 Recent data pulled", res);
+			this.setState({
+				campers: res
+			});
+		});
+
 	}
 	
 	render () {
+		
+		var listCampers = () => {
+			if (this.state.campers.length === 0) { return; }
+			console.log(this.state.campers[0].username);
+			return this.state.campers.map((camper) => {
+					return (<p>{camper.username}</p>);
+			})
+		};
 
 		return (
 			<div className="container">
-				<h1>Leaderboard goes here</h1>	
-				
+				<h1>Leaderboard goes here</h1>						
+				{listCampers()}
 			</div>
 		);	
 	}
