ba8142362211107f81c0e5c52f9e962012f0173a
Coy Sanders <coymeetsworld@gmail.com>
2017-01-03 16:53:03 -0800

    Template component and style files created

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import HeaderBar from 'HeaderBar';
+import Leaderboard from 'Leaderboard';
+import FooterBar from 'FooterBar';
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
 	<div>
+		<HeaderBar/>
+		<Leaderboard/>	
+		<FooterBar/>
 	</div>,
 	document.getElementById('app')
 );
---
+++ app/components/Leaderboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Leaderboard extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+	
+	render () {
+		return (
+			<div className="container">
+				<h1>Leaderboard goes here</h1>	
+			</div>
+		);	
+	}
+}
+
+export default Leaderboard;
