6e1fd1670222f018ae2f0585034247d3159faa13
Coy Sanders <coymeetsworld@gmail.com>
2017-01-03 16:35:50 -0800

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+	<div>
+	</div>,
+	document.getElementById('app')
+);
